b03b5dcf69701d9a5f921136be1f3971de4d47bc
Greg Kraehenbuehl <chioxin@gmail.com>
2018-12-31 12:34:12 -0700

    Completed JTHW Excercise 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main(String[] args) {
+    System.out.println("+--------------------------+");
+    System.out.println("|                          |");
+    System.out.println("|      CORNER STORE        |");
+    System.out.println("|                          |");
+    System.out.println("| 2018-12-31  12:30PM      |");
+    System.out.println("|                          |");
+    System.out.println("| Gallons:         10.870  |");
+    System.out.println("| Price/gallon:   $ 1.890  |");
+    System.out.println("|                          |");
+    System.out.println("| Fuel total:    $ 20.54   |");
+    System.out.println("|                          |");
+    System.out.println("+--------------------------+");
+  }
+}
