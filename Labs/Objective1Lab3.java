d31404c4e1a3eb4c4079a518112f1e46b3836827
Greg Kraehenbuehl <chioxin@gmail.com>
2018-12-31 13:30:20 -0700

    Completed Objective1Lab3, Question in Comments

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,10 @@
+public class Objective1Lab3 {
+  public static void main ( String[] args) {
+    System.out.println ( "Name : Gregory Kraehenbuehl");
+    System.out.println ( "Age : 32" );
+    System.out.println ( "State : Florida");
+    /* Is this bad/ I prefer doing it above, as opposed to using \n as
+    it is just that much easier to read? I tried doing it the other way
+    in Objective1Lab2 an I did not think it was readable honestly. */
+  }
+}
