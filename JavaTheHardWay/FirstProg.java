08d107a71a473b83456906869a0fe5aa621a1aa9
Greg Kraehenbuehl <chioxin@gmail.com>
2018-12-31 12:17:06 -0700

    added study drill to FirstProg.java

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,7 +1,7 @@
 public class FirstProg {
   public static void main(String[] args) {
-    System.out.println("I am determined that you will learn code");
-    System.out.println("Today is a great day");
+    System.out.println("I, Gregory Kraehenbuehl, am determined that you will learn code");
+    System.out.println("Today's date is Monday, December 31, 2018");
   }
-  
+
 }
