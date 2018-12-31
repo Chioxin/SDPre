a8384eb29bbe0c1ef38771433658accc9a73bee7
Greg Kraehenbuehl <chioxin@gmail.com>
2018-12-31 11:34:12 -0700

    Finish JTHW Ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+  public static void main(String[] args) {
+    System.out.println("I am determined that you will learn code");
+    System.out.println("Today is a great day");
+  }
+  
+}
